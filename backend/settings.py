4f2ba88a4751c5dbb9d59c6fc07d16408896434e
Volodymyr Hubash <vhubash@gmail.com>
2022-06-24 14:36:13 +0300

    updt

--- backend/settings.py
+++ backend/settings.py
@@ -120,25 +120,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'proshop',
-#        'USER': 'dennisivy',
-#        'PASSWORD': os.environ.get('DB_PASS'),
-#        'HOST': os.environ.get('HOST'),
-#        'PORT': '5432'
-#    }
-# }
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'technoshop-db',
+       'USER': 'vhubash',
+       'PASSWORD': os.environ.get('DB_PASS'),
+       'HOST': os.environ.get('DATABASE_HOST'),
+       'PORT': '5432'
+   }
+}
 
 
 # Password validation
