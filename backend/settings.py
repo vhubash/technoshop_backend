d3935886e8eea97303f8cfa2889261b185a26807
Volodymyr Hubash <vhubash@gmail.com>
2022-06-27 08:35:13 +0300

    media url fix

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'sc%oco$+(3$z$at=z4j)#l-+ym)+_b11389mdt2^12m$bf63%@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False   
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'technoshop-frontend.vercel.app', 'technoshop-hubash.herokuapp.com', 'technoshop-django.azurewebsites.net', 'technoshop-db.postgres.database.azure.com']
+ALLOWED_HOSTS = ['localhost', 'technoshop-frontend.vercel.app', 'technoshop-hubash.herokuapp.com', 'technoshop-db.postgres.database.azure.com']
 
 
 # Application definition
@@ -185,7 +185,7 @@ STATICFILES_DIRS = [
 ]
 
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / '/static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 CORS_ALLOW_ALL_ORIGINS = True
